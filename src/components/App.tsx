f5d7d5091fb8c606c006bf9bea58f0a772f496d7
Kranti <darkphoton20@gmail.com>
2022-01-16 16:07:59 +0800

    Completed intial chat render and update functionality.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 // import { io } from 'socket.io-client';
 import { io } from 'socket.io-client';
 
@@ -7,7 +7,8 @@ import '../css/App.css';
 import { CHAT_SERVER_URL } from '../env';
 
 const App: React.FC = () => {
-  const [messages, setMessages] = useState<string[]>();
+  const [messages, setMessages] = useState<string[]>([]);
+  const msgRef = useRef<HTMLDivElement>();
 
   // setup socket.io server
   const connectChatServer = () => {
@@ -19,16 +20,19 @@ const App: React.FC = () => {
   useEffect(() => {
     const socket = connectChatServer();
     socket.onAny((message) => {
-      console.log('Message:', message);
-      if (messages) {
-        setMessages([...messages, message]);
-      }
+      // console.log('Message:', message);
+      // if (messages) {
+      setMessages((m) => [...m, message]);
+      // }
     });
   }, []);
+
+  console.log('all messages:', messages);
 
   return (
     <div>
       <h3>LiveChat App</h3>
+      <div className="options"></div>
       <div className="chat-area">
         {messages?.map((msg) => (
           <div className="msg-item">{msg}</div>
