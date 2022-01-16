2a1b194e91eb24291f298bc0851130fb30f411c6
Kranti <darkphoton20@gmail.com>
2022-01-16 16:47:11 +0800

    Styling chat area and adding nightmode.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -8,6 +8,7 @@ import { CHAT_SERVER_URL } from '../env';
 
 const App: React.FC = () => {
   const [messages, setMessages] = useState<string[]>([]);
+  const [nightmode, setNightMode] = useState<boolean>(false);
   const msgRef = useRef<HTMLDivElement>();
 
   // setup socket.io server
@@ -20,20 +21,29 @@ const App: React.FC = () => {
   useEffect(() => {
     const socket = connectChatServer();
     socket.onAny((message) => {
-      // console.log('Message:', message);
-      // if (messages) {
+      // render message on dom on when recieving socket messages
       setMessages((m) => [...m, message]);
-      // }
     });
   }, []);
 
-  console.log('all messages:', messages);
-
   return (
-    <div>
+    <div className={nightmode ? 'app nightmode' : 'app daymode'}>
       <h3>LiveChat App</h3>
-      <div className="options"></div>
+      <div className="options">
+        <div className="options-title">Settings:</div>
+        <div className="options-item">
+          <label htmlFor="nightmode">Night Mode</label>
+          <input
+            type="checkbox"
+            name="nightmode"
+            checked={nightmode}
+            onChange={(e) => setNightMode((nightmode) => !nightmode)}
+          />
+        </div>
+      </div>
+      <hr />
       <div className="chat-area">
+        <div className="chat-title">Chat Area</div>
         {messages?.map((msg) => (
           <div className="msg-item">{msg}</div>
         ))}
